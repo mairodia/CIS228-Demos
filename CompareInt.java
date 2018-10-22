76ae07e13a17bce979cd18c0ab7081cb9f2a2fd2
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-10-22 16:40:44 -0700

    Create CompareInt.java

---
+++ CompareInt.java
@@ -0,0 +1,16 @@
+public class CompareInt 
+{
+	public static void main(String[] args) 
+	{
+		int x = 10;
+		int y = 13;
+		
+		if(x>y)
+			System.out.println("x > y");
+		else 
+		if(x<y)
+			System.out.println("x < y");
+		else
+			System.out.println("x equals y");
+	}
+}
