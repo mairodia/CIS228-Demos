bf4c38e90b495a2b9fd198b796a6a6d6ffdebc96
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-10-22 16:37:07 -0700

    Create DisplayInfo.java

---
+++ DisplayInfo.java
@@ -0,0 +1,13 @@
+public interface DisplayInfo 
+{
+	void displayName();
+	void displayAddress();
+	void displayName1();
+	void displayAddress1();
+	void displayName2();
+	void displayAddress2();
+	void displayName3();
+	void displayAddress3();
+	void displayName4();
+	void displayAddress4();
+}
