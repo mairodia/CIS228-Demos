81d93a4f83364c4b2423eeeb03f898c50e8ce137
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-10-22 16:36:25 -0700

    Create SelectionSortDemo.java

---
+++ SelectionSortDemo.java
@@ -0,0 +1,25 @@
+public class SelectionSortDemo 
+{
+	public static void selectionSort(int[] array)
+	{
+			int star4tScan, index, minIndex, minValue;
+			
+			for(startScan = 0; startScan < (array.length-1); startScan++)
+			{
+				minIndex = startScan;
+				minValue = array[startScan];
+				
+				for(index = startScan+1; index < array.length; index++)
+				{
+					if(array[index] < minValue)
+					{
+						minValue = array[index]; minIndex = index;
+					}
+				}
+				
+				array[minIndex] = array[startScan];
+				array[startScan] = minValue;
+			}
+			
+	}
+}
